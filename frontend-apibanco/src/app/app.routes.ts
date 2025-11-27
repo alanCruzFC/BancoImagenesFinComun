84b03f04ccc1180a1b92745d8e2f08e0a65a726d
alanCruzFC <acruzdelarosa.e@fincomun.com.mx>
2025-11-27 13:02:54 -0600

    Changes to be committed: 	modified:   frontend-apibanco/src/app/app.routes.ts

--- frontend-apibanco/src/app/app.routes.ts
+++ frontend-apibanco/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { AuthGuard } from './core/auth.guard';
 import { ImagenesRegistro } from './registros/imagenes/imagenes';
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'visualizar/:numeroSolicitud', component: ImagenesRegistro },
   {
     path: 'login',
