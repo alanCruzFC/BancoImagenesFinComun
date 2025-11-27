6827de9bcbae8db6f4dc6c0047eaa4ce2a6aa89e
alanCruzFC <acruzdelarosa.e@fincomun.com.mx>
2025-11-27 13:15:03 -0600

    Changes to be committed: 	modified:   frontend-apibanco/src/app/app.routes.server.ts 	modified:   frontend-apibanco/src/app/app.routes.ts

--- frontend-apibanco/src/app/app.routes.server.ts
+++ frontend-apibanco/src/app/app.routes.server.ts
@@ -1,17 +1,10 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  // raíz → prerender (redirige a login en cliente)
   { path: '', renderMode: RenderMode.Prerender },
-
-  // estáticas
   { path: 'login', renderMode: RenderMode.Prerender },
   { path: 'dashboard', renderMode: RenderMode.Prerender },
-
-  // dinámicas → Server (no prerender)
   { path: 'visualizar/:numeroSolicitud', renderMode: RenderMode.Server },
   { path: 'dashboard/visualizar/:numeroSolicitud', renderMode: RenderMode.Server },
-
-  // catch‑all → Server
   { path: '**', renderMode: RenderMode.Server }
 ];
--- frontend-apibanco/src/app/app.routes.ts
+++ frontend-apibanco/src/app/app.routes.ts
@@ -3,13 +3,8 @@ import { AuthGuard } from './core/auth.guard';
 import { ImagenesRegistro } from './registros/imagenes/imagenes';
 
 export const routes: Routes = [
-  // raíz → redirige a login
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-
-  // dinámicas
+  { path: '', loadComponent: () => import('./auth/login/login').then(m => m.LoginComponent) },
   { path: 'visualizar/:numeroSolicitud', component: ImagenesRegistro },
-
-  // estáticas
   {
     path: 'login',
     loadComponent: () => import('./auth/login/login').then(m => m.LoginComponent)
@@ -19,7 +14,6 @@ export const routes: Routes = [
     canActivate: [AuthGuard],
     loadChildren: () => import('./dashboard/dashboard.routes').then(m => m.dashboardRoutes)
   },
-
-  // catch‑all
   { path: '**', redirectTo: 'dashboard', pathMatch: 'full' }
 ];
+
